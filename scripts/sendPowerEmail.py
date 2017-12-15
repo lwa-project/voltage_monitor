abdba3399340093f5fca017f199e28a8929adc33
Jayce Dowell <jayce.dowell@gmail.com>
2017-12-15 23:38:45 +0000

    Update the alert e-mail address for sendPowerEmail.py.

--- scripts/sendPowerEmail.py
+++ scripts/sendPowerEmail.py
@@ -31,7 +31,7 @@ dataRE = re.compile(r'^\[(?P<date>.*)\] (?P<type>[A-Z0-9]*): (?P<data>.*)$')
 SITE = gethostname().split('-', 1)[0]
 
 # E-mail Users
-TO = ['jdowell@unm.edu',]#'lwa1ops@phys.unm.edu',]
+TO = ['lwa1ops@phys.unm.edu',]
 
 # SMTP user and password
 #if SITE == 'lwa1':
