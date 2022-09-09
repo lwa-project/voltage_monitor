6b24fba338cb28e33be9c40c8a24430fffce8d87
jaycedowell <jayce.dowell@gmail.com>
2022-09-09 13:46:26 -0600

    Don't daemonize with systemd.

--- scripts/sendPowerEmail.py
+++ scripts/sendPowerEmail.py
@@ -61,76 +61,6 @@ UTC = pytz.utc
 MST = pytz.timezone('US/Mountain')
 
 
-"""
-This module is used to fork the current process into a daemon.
-Almost none of this is necessary (or advisable) if your daemon
-is being started by inetd. In that case, stdin, stdout and stderr are
-all set up for you to refer to the network connection, and the fork()s
-and session manipulation should not be done (to avoid confusing inetd).
-Only the chdir() and umask() steps remain as useful.
-
-From:
-  http://code.activestate.com/recipes/66012-fork-a-daemon-process-on-unix/
-
-References:
-  UNIX Programming FAQ
-    1.7 How do I get my program to act like a daemon?
-        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-
-    Advanced Programming in the Unix Environment
-      W. Richard Stevens, 1992, Addison-Wesley, ISBN 0-201-56317-7.
-"""
-
-def daemonize(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
-    """
-    This forks the current process into a daemon.
-    The stdin, stdout, and stderr arguments are file names that
-    will be opened and be used to replace the standard file descriptors
-    in sys.stdin, sys.stdout, and sys.stderr.
-    These arguments are optional and default to /dev/null.
-    Note that stderr is opened unbuffered, so
-    if it shares a file with stdout then interleaved output
-    may not appear in the order that you expect.
-    """
-    
-    # Do first fork.
-    try:
-        pid = os.fork()
-        if pid > 0:
-            sys.exit(0) # Exit first parent.
-    except OSError as e:
-        sys.stderr.write("fork #1 failed: (%d) %s\n" % (e.errno, e.strerror))
-        sys.exit(1)
-
-    # Decouple from parent environment.
-    os.chdir("/")
-    os.umask(0)
-    os.setsid()
-
-    # Do second fork.
-    try:
-        pid = os.fork()
-        if pid > 0:
-            sys.exit(0) # Exit second parent.
-    except OSError as e:
-        sys.stderr.write("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror))
-        sys.exit(1)
-
-    # Now I am a daemon!
-
-    # Redirect standard file descriptors.
-    si = open(stdin, 'r')
-    so = open(stdout, 'a+')
-    se = open(stderr, 'a+')
-    ## Make a time mark
-    mark = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    so.write("===\nLaunched at %s\n===\n" % mark)
-    se.write("===\nLaunched at %s\n===\n" % mark)
-    os.dup2(si.fileno(), sys.stdin.fileno())
-    os.dup2(so.fileno(), sys.stdout.fileno())
-    os.dup2(se.fileno(), sys.stderr.fileno())
-
-
 def get_uptime():
     """
     Determine and return the current uptime in minutes.
@@ -404,8 +334,6 @@ if __name__ == "__main__":
                         help='file to write the current PID to')
     args = parser.parse_args()
     
-    daemonize('/dev/null','/tmp/spe-stdout','/tmp/spe-stderr')
-    
     # PID file
     if args.pid_file is not None:
         fh = open(args.pid_file, 'w')
@@ -413,4 +341,3 @@ if __name__ == "__main__":
         fh.close()
         
     DLVM(mcastAddr=args.address, mcastPort=args.port)
-    
--- voltageMonitor.py
+++ voltageMonitor.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from __future__ import print_function
-
 """
 lineMonitor.py - Python script for interfacing with a TekPower TP4000ZC DMM
 and logging the AC voltages out at the site.
@@ -45,76 +43,6 @@ if not os.path.exists(STATE_DIR):
 else:
     if not os.path.isdir(STATE_DIR):
         raise RuntimeError("'%s' is not a directory" % STATE_DIR)
-
-
-"""
-This module is used to fork the current process into a daemon.
-Almost none of this is necessary (or advisable) if your daemon
-is being started by inetd. In that case, stdin, stdout and stderr are
-all set up for you to refer to the network connection, and the fork()s
-and session manipulation should not be done (to avoid confusing inetd).
-Only the chdir() and umask() steps remain as useful.
-
-From:
-  http://code.activestate.com/recipes/66012-fork-a-daemon-process-on-unix/
-
-References:
-  UNIX Programming FAQ
-    1.7 How do I get my program to act like a daemon?
-        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-
-    Advanced Programming in the Unix Environment
-      W. Richard Stevens, 1992, Addison-Wesley, ISBN 0-201-56317-7.
-"""
-
-def daemonize(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
-    """
-    This forks the current process into a daemon.
-    The stdin, stdout, and stderr arguments are file names that
-    will be opened and be used to replace the standard file descriptors
-    in sys.stdin, sys.stdout, and sys.stderr.
-    These arguments are optional and default to /dev/null.
-    Note that stderr is opened unbuffered, so
-    if it shares a file with stdout then interleaved output
-    may not appear in the order that you expect.
-    """
-    
-    # Do first fork.
-    try:
-        pid = os.fork()
-        if pid > 0:
-            sys.exit(0) # Exit first parent.
-    except OSError as e:
-        sys.stderr.write("fork #1 failed: (%d) %s\n" % (e.errno, e.strerror))
-        sys.exit(1)
-
-    # Decouple from parent environment.
-    os.chdir("/")
-    os.umask(0)
-    os.setsid()
-
-    # Do second fork.
-    try:
-        pid = os.fork()
-        if pid > 0:
-            sys.exit(0) # Exit second parent.
-    except OSError as e:
-        sys.stderr.write("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror))
-        sys.exit(1)
-
-    # Now I am a daemon!
-
-    # Redirect standard file descriptors.
-    si = open(stdin, 'r')
-    so = open(stdout, 'a+')
-    se = open(stderr, 'a+')
-    ## Make a time mark
-    mark = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    so.write("===\nLaunched at %s\n===\n" % mark)
-    se.write("===\nLaunched at %s\n===\n" % mark)
-    os.dup2(si.fileno(), sys.stdin.fileno())
-    os.dup2(so.fileno(), sys.stdout.fileno())
-    os.dup2(se.fileno(), sys.stderr.fileno())
 
 
 class DuplicateFilter(logging.Filter):
@@ -473,16 +401,10 @@ if __name__ == "__main__":
                         help='file to log operational status to')
     parser.add_argument('-d', '--debug', action='store_true',
                         help='print debug messages as well as info and higher')
-    parser.add_argument('-f', '--foreground', action='store_true',
-                        help='run in the foreground, do not daemonize')
     args = parser.parse_args()
     
     # Parse the configuration file
     with open(args.config_file, 'r') as ch:
         args.config_file = json.loads(json_minify.json_minify(ch.read()))
         
-    if not args.foreground:
-        daemonize('/dev/null','/tmp/lm-stdout','/tmp/lm-stderr')
-        
     main(args)
-    
